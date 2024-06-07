7277763750ee2f771ce4fe42a3de9a8b0d5807a7
mdhenry763 <mdhenry763@gmail.com>
2024-06-07 20:39:02 +0200

    chore(gameplay): adding comments.

--- GADE_7321_Part-2_Group-DM/Assets/!!Scripts/AI_Mike/Minimax/UtilityFunctionManager.cs
+++ GADE_7321_Part-2_Group-DM/Assets/!!Scripts/AI_Mike/Minimax/UtilityFunctionManager.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class UtilityFunctionManager : MonoBehaviour
 {
-    public float UtilityFunction(string[,] board, string currentPlayerColor)
+    public float UtilityFunction(string[,] board, string currentPlayerColour) //Add a default value
     {
-        int w1 = 1, w2 = 1, w3 = 1, w4 = 1, w5 = 1;
+        const float w1 = 1, w2 = 0.6f, w3 = 1, w4 = 0.7f, w5 = 1;
 
-        int pieceCount = PieceCount(board, currentPlayerColor);
-        int diagonalControl = DiagonalControl(board, currentPlayerColor);
-        int vulnerability = Vulnerability(board, currentPlayerColor);
-        int strategicPositions = StrategicPositions(board, currentPlayerColor);
-        int highValuePosition = HighValuePosition(board, currentPlayerColor);
+        int pieceCount = PieceCount(board, currentPlayerColour);
+        int diagonalControl = DiagonalControl(board, currentPlayerColour);
+        int vulnerability = Vulnerability(board, currentPlayerColour);
+        int strategicPositions = StrategicPositions(board, currentPlayerColour);
+        int highValuePosition = HighValuePosition(board, currentPlayerColour);
 
         return w1 * pieceCount + w2 * diagonalControl - w3 * vulnerability + w4 * strategicPositions + w5 * highValuePosition;
     }
@@ -27,9 +27,14 @@ public class UtilityFunctionManager : MonoBehaviour
         return count;
     }
 
-    private int DiagonalControl(string[,] board, string color)
+    private int DiagonalControl(string[,] board, string color) //Do I not need the position getting evaluated?
     {
         //regular top left, top right bottom, bottom right = 5
+
+        bool isHighDiagonalControl =
+            board[0, 0] == "_" || board[0, 4] == "_" || board[4, 0] == "_" || board[4, 4] == "_";
+
+        if (isHighDiagonalControl) return 100;
         
         return 0; // Placeholder
     }
@@ -44,6 +49,15 @@ public class UtilityFunctionManager : MonoBehaviour
     private int StrategicPositions(string[,] board, string color)
     {
         //regular top left, top right bottom, bottom right = 5
+        List<Vector2> edgePieces = new List<Vector2>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            edgePieces.Add(new Vector2(0,i)); //top row
+            edgePieces.Add(new Vector2(i, 0)); //left column
+            edgePieces.Add(new Vector2(4, i)); //bottom row
+            edgePieces.Add(new Vector2(i, 4)); //right column
+        }
         //other edge pieces = 3
         return 0; // Placeholder
     }
